4bb172021185462b20a2ff1360323a35928aa010
Treasure Kabareebe <treasurecalm@gmail.com>
2020-10-05 14:15:04 +0300

    Add re-usable writeToLog function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,19 +13,28 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
   outputResult(currentResult, calcDescription); // from vendor file
 }
 
+function writeToLog(
+  operationIdentifier, 
+  prevResult, 
+  operationNumber, 
+  newResult
+){
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
 function add(){
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult += enteredNumber;
   createAndWriteOutput('+', initialResult, enteredNumber);
-  const logEntry = {
-    operation: 'ADD',
-    prevResult: initialResult,
-    number: enteredNumber,
-    result: currentResult
-  };
-  logEntries.push(logEntry);
-  console.log(logEntries);
+  writeToLog('ADD', initialResult, enteredNumber, currentResult);
 }
 
 function subtract(){
@@ -33,6 +42,7 @@ function subtract(){
   const initialResult = currentResult;
   currentResult -= enteredNumber;
   createAndWriteOutput('-', initialResult, enteredNumber);
+  writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
 }
 
 function multiply(){
@@ -40,6 +50,7 @@ function multiply(){
   const initialResult = currentResult;
   currentResult *= enteredNumber;
   createAndWriteOutput('*', initialResult, enteredNumber);
+  writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
 }
 
 function divide(){
@@ -47,6 +58,7 @@ function divide(){
   const initialResult = currentResult;
   currentResult /= enteredNumber;
   createAndWriteOutput('/', initialResult, enteredNumber);
+  writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
 }
 
 addBtn.addEventListener('click', add);
