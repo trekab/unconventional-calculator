ade12ab754d2b442032bf1114c9268199cfb11d7
Treasure Kabareebe <treasurecalm@gmail.com>
2020-10-05 13:35:21 +0300

    Add comments for extra context

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// Gets input from input field
 function getUserNumberInput(){
   return parseInt(userInput.value);
 }
 
+// Generates and writes calculation log
 function createAndWriteOutput(operator, resultBoforeCalc, calcNumber){
   const calcDescription = `${resultBoforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResult, calcDescription);
+  outputResult(currentResult, calcDescription); // from vendor file
 }
 
 function add(){
